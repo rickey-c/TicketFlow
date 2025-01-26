3c6a19c239705e1e643b6db84ef7554e5b1f52ab
rickey-c <1604120019@qq.com>
2025-01-26 17:09:56 +0800

    fix(common): fix DEFAULT_PREFIX_DISTINCTION_NAME

--- ticketflow-common/src/main/java/com/ticketflow/constant/Constant.java
+++ ticketflow-common/src/main/java/com/ticketflow/constant/Constant.java
@@ -29,7 +29,7 @@ public class Constant {
 
     public static final String PREFIX_DISTINCTION_NAME = "prefix.distinction.name";
 
-    public static final String DEFAULT_PREFIX_DISTINCTION_NAME = "damai";
+    public static final String DEFAULT_PREFIX_DISTINCTION_NAME = "ticketflow";
 
     public static final String SPRING_INJECT_PREFIX_DISTINCTION_NAME = "${" + PREFIX_DISTINCTION_NAME + ":" + DEFAULT_PREFIX_DISTINCTION_NAME + "}";
 
