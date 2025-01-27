1af99dd26be1b73a07710773917e249f9b498e77
rickey-c <1604120019@qq.com>
2025-01-28 01:58:05 +0800

    fix(thread-pool-autoConfig): 补充"@Configuration"注解

--- ticketflow-thread-pool-framework/src/main/java/com/ticketflow/filter/FilterConfig.java
+++ ticketflow-thread-pool-framework/src/main/java/com/ticketflow/filter/FilterConfig.java
@@ -1,13 +1,15 @@
 package com.ticketflow.filter;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
- * @Description:
+ * @Description: 过滤器配置
  * @Author: rickey-c
  * @Date: 2025/1/23 15:32
  */
+@Configuration
 public class FilterConfig {
     
     @Bean
