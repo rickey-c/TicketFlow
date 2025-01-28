66b4ff38250473b9820fe14379c59d29ed326265
rickey-c <1604120019@qq.com>
2025-01-29 02:33:28 +0800

    feat(common): 新增RuleStatus枚举

---
+++ ticketflow-common/src/main/java/com/ticketflow/enums/RuleStatus.java
@@ -0,0 +1,58 @@
+package com.ticketflow.enums;
+
+/**
+ * @Description: 规则状态码
+ * @Author: rickey-c
+ * @Date: 2025/1/29 00:32
+ */
+public enum RuleStatus {
+    /**
+     * 规则状态
+     * */
+    RUN(1,"正常"),
+    STOP(0,"禁用")
+    ;
+
+    private Integer code;
+
+    private String msg;
+
+    RuleStatus(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return this.msg == null ? "" : this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public static String getMsg(Integer code) {
+        for (RuleStatus re : RuleStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re.msg;
+            }
+        }
+        return "";
+    }
+
+    public static RuleStatus getRc(Integer code) {
+        for (RuleStatus re : RuleStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re;
+            }
+        }
+        return null;
+    }
+}
