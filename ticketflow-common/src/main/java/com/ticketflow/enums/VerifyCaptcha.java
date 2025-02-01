7e04f853109dd68ed2dbaa8d1de69dcc0d4f6219
rickey-c <1604120019@qq.com>
2025-02-01 16:48:53 +0800

    feat(common): 新增验证码枚举类

---
+++ ticketflow-common/src/main/java/com/ticketflow/enums/VerifyCaptcha.java
@@ -0,0 +1,29 @@
+package com.ticketflow.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @Description: 校验是否需要验证码
+ * @Author: rickey-c
+ * @Date: 2025/2/1 16:34
+ */
+@AllArgsConstructor
+public enum VerifyCaptcha {
+
+    NO(0, "no", "不需要"),
+
+    YES(1, "yes", "需要"),
+    ;
+
+    @Getter
+    private Integer code;
+    
+    @Getter
+    private String value;
+    
+    @Getter
+    private String msg;
+
+
+}
