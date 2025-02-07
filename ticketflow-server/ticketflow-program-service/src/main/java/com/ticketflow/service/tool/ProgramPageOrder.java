7bc522c3803278941afd5a544020b0f76e3237d1
rickey-c <1604120019@qq.com>
2025-02-07 23:53:39 +0800

    fix(program-service): 修复分页字段

--- ticketflow-server/ticketflow-program-service/src/main/java/com/ticketflow/service/tool/ProgramPageOrder.java
+++ ticketflow-server/ticketflow-program-service/src/main/java/com/ticketflow/service/tool/ProgramPageOrder.java
@@ -1,12 +1,15 @@
 package com.ticketflow.service.tool;
 
-import javax.swing.*;
+import lombok.Data;
+import org.elasticsearch.search.sort.SortOrder;
+
 
 /**
  * @Description: 节目排序实体
  * @Author: rickey-c
  * @Date: 2025/2/3 20:31
  */
+@Data
 public class ProgramPageOrder {
 
     public String sortParam;
