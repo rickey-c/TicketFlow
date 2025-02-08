435b99afed6057c05bad452bcb10b570bd81934c
rickey-c <1604120019@qq.com>
2025-02-09 00:09:26 +0800

    fix(program-service): 修复购票组合校验

---
+++ ticketflow-server/ticketflow-program-service/src/main/java/com/ticketflow/service/init/ProgramDocumentParamName.java
@@ -0,0 +1,54 @@
+package com.ticketflow.service.init;
+
+/**
+ * @Description: 节目文档参数名
+ * @Author: rickey-c
+ * @Date: 2025/2/7 23:27
+ */
+public class ProgramDocumentParamName {
+
+    public static final String INDEX_NAME = "program";
+
+    public static final String INDEX_TYPE = "program";
+
+    public static final String ID = "id";
+
+    public static final String PROGRAM_GROUP_ID = "programGroupId";
+
+    public static final String PRIME = "prime";
+
+    public static final String TITLE = "title";
+
+    public static final String ACTOR = "actor";
+
+    public static final String PLACE = "place";
+
+    public static final String ITEM_PICTURE = "itemPicture";
+
+    public static final String AREA_ID = "areaId";
+
+    public static final String AREA_NAME = "areaName";
+
+    public static final String PROGRAM_CATEGORY_ID = "programCategoryId";
+
+    public static final String PROGRAM_CATEGORY_NAME = "programCategoryName";
+
+    public static final String PARENT_PROGRAM_CATEGORY_ID = "parentProgramCategoryId";
+
+    public static final String PARENT_PROGRAM_CATEGORY_NAME = "parentProgramCategoryName";
+
+    public static final String HIGH_HEAT = "high_heat";
+
+    public static final String ISSUE_TIME = "issueTime";
+
+    public static final String SHOW_TIME = "showTime";
+
+    public static final String SHOW_DAY_TIME = "showDayTime";
+
+    public static final String SHOW_WEEK_TIME = "showWeekTime";
+
+    public static final String MIN_PRICE = "minPrice";
+
+    public static final String MAX_PRICE = "maxPrice";
+
+}
