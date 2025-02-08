fbc0ba00659da69ca96e9249b4586eb46fff4d51
rickey-c <1604120019@qq.com>
2025-02-09 00:10:00 +0800

    fix(program-service): 修复购票组合校验

--- ticketflow-server/ticketflow-program-service/src/main/java/com/ticketflow/service/composite/ProgramRecommendCheckHandler.java
+++ ticketflow-server/ticketflow-program-service/src/main/java/com/ticketflow/service/composite/ProgramRecommendCheckHandler.java
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Component;
 import java.util.Objects;
 
 /**
- * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料
- * @description: 节目推荐参数验证
- * @author: 阿星不是程序员
- **/
+ * @Description: 节目详情查询验证
+ * @Author: rickey-c
+ * @Date: 2025/2/8 23:47
+ */
 @Component
 public class ProgramRecommendCheckHandler extends AbstractComposite<ProgramRecommendListDto> {
 
