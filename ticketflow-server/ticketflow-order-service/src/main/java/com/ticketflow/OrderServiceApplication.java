ef102fb9127ea21e6495706d2221718acf9f6c02
rickey-c <1604120019@qq.com>
2025-02-09 23:28:53 +0800

    fix(order-service): 修复启动类

--- ticketflow-server/ticketflow-order-service/src/main/java/com/ticketflow/OrderServiceApplication.java
+++ ticketflow-server/ticketflow-order-service/src/main/java/com/ticketflow/OrderServiceApplication.java
@@ -1,9 +1,15 @@
 package com.ticketflow;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@MapperScan({"com.ticketflow.mapper"})
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"com.ticketflow","com.baidu.fsg.uid"})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
