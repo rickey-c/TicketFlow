3b6613ba2a797f4f4394dd4374300fee35cb7ea5
rickey-c <1604120019@qq.com>
2025-02-09 15:29:01 +0800

    feat(program-service): 新增订单状态枚举

---
+++ ticketflow-common/src/main/java/com/ticketflow/enums/OrderStatus.java
@@ -0,0 +1,60 @@
+package com.ticketflow.enums;
+
+/**
+ * @Description: 支付状态枚举
+ * @Author: rickey-c
+ * @Date: 2025/2/9 14:18
+ */
+public enum OrderStatus {
+    /**
+     * 订单状态
+     * */
+    NO_PAY(1,"未支付"),
+    CANCEL(2,"已取消"),
+    PAY(3,"已支付"),
+    REFUND(4,"已退单"),
+    ;
+
+    private Integer code;
+
+    private String msg;
+
+    OrderStatus(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return this.msg == null ? "" : this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public static String getMsg(Integer code) {
+        for (OrderStatus re : OrderStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re.msg;
+            }
+        }
+        return "";
+    }
+
+    public static OrderStatus getRc(Integer code) {
+        for (OrderStatus re : OrderStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re;
+            }
+        }
+        return null;
+    }
+}
