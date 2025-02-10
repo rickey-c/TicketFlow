ae70fafef508a53a953007d99c501e03c4ce3809
rickey-c <1604120019@qq.com>
2025-02-10 14:45:49 +0800

    refactor(pay-service): 启动类命名、依赖更新

--- ticketflow-server/ticketflow-pay-service/src/main/java/com/ticketflow/PayApplication.java
+++ ticketflow-server/ticketflow-pay-service/src/main/java/com/ticketflow/PayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketflowPayServiceApplication {
+public class PayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketflowPayServiceApplication.class, args);
+        SpringApplication.run(PayApplication.class, args);
     }
 
 }
