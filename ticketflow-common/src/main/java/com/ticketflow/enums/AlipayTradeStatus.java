cd8bb1e9fc38a105c786d22147fb62d5db91ef1b
rickey-c <1604120019@qq.com>
2025-02-10 14:44:53 +0800

    feat(common): 新增支付相关枚举类

---
+++ ticketflow-common/src/main/java/com/ticketflow/enums/AlipayTradeStatus.java
@@ -0,0 +1,75 @@
+package com.ticketflow.enums;
+
+/**
+ * @Description: 支付宝相关信息
+ * @Author: rickey-c
+ * @Date: 2025/2/10 11:45
+ */
+public enum AlipayTradeStatus {
+    /**
+     * 支付宝相关信息
+     * */
+    WAIT_BUYER_PAY(1,"wait_buyer_pay","交易创建，等待买家付款"),
+
+    TRADE_CLOSED(2,"trade_closed","未付款交易超时关闭，或支付完成后全额退款"),
+
+    TRADE_SUCCESS(3,"TRADE_SUCCESS","交易支付成功"),
+
+    TRADE_FINISHED(4,"TRADE_FINISHED","交易结束，不可退款"),
+    ;
+
+    private Integer code;
+
+    private String value;
+
+    private String msg;
+
+    AlipayTradeStatus(Integer code, String value, String msg) {
+        this.code = code;
+        this.value = value;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return this.msg == null ? "" : this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(final String value) {
+        this.value = value;
+    }
+
+    public static String getMsg(Integer code) {
+        for (AlipayTradeStatus re : AlipayTradeStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re.msg;
+            }
+        }
+        return "";
+    }
+
+    public static AlipayTradeStatus getRc(Integer code) {
+        for (AlipayTradeStatus re : AlipayTradeStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re;
+            }
+        }
+        return null;
+    }
+}
+
---
+++ ticketflow-common/src/main/java/com/ticketflow/enums/PayBillStatus.java
@@ -0,0 +1,60 @@
+package com.ticketflow.enums;
+
+/**
+ * @Description: 支付账单状态
+ * @Author: rickey-c
+ * @Date: 2025/2/10 11:46
+ */
+public enum PayBillStatus {
+    /**
+     * 支付账单状态
+     * */
+    NO_PAY(1,"未支付"),
+    CANCEL(2,"已取消"),
+    PAY(3,"已支付"),
+    REFUND(4,"已退单"),
+    ;
+
+    private Integer code;
+
+    private String msg;
+
+    PayBillStatus(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return this.msg == null ? "" : this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public static String getMsg(Integer code) {
+        for (PayBillStatus re : PayBillStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re.msg;
+            }
+        }
+        return "";
+    }
+
+    public static PayBillStatus getRc(Integer code) {
+        for (PayBillStatus re : PayBillStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re;
+            }
+        }
+        return null;
+    }
+}
